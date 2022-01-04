44a9ab0f1b1b8b9f5974d1932f0d8bfcb21ecf4a
Rusewww <rusewww1@gmail.com>
2022-01-04 15:32:22 +0200

    fix

--- src/mx_list_file_long.c
+++ src/mx_list_file_long.c
@@ -55,20 +55,21 @@ char *mx_list_file_long(char *src, int *block_count) {
     lstat(src, &file_info);
     mode = file_info.st_mode;
     *block_count += file_info.st_blocks;
-    if (S_ISREG(mode))
+    if (S_ISREG(mode)) {
         mx_str_concat(&res, "-");
-    else if (S_ISDIR(mode))
+    } else if (S_ISDIR(mode)) {
         mx_str_concat(&res, "d");
-    else if (S_ISLNK(mode))
+    } else if (S_ISLNK(mode)) {
         mx_str_concat(&res, "l");
-    else if (S_ISSOCK(mode))
+    } else if (S_ISSOCK(mode)) {
         mx_str_concat(&res, "s");
-    else if (S_ISCHR(mode))
+    } else if (S_ISCHR(mode)) {
         mx_str_concat(&res, "c");
-    else if (S_ISFIFO(mode))
+    } else if (S_ISFIFO(mode)) {
         mx_str_concat(&res, "p");
-    else if (S_ISBLK(mode))
+    } else if (S_ISBLK(mode)) {
         mx_str_concat(&res, "b");
+    }
     temp_buf = get_permitions(mode);
     mx_str_concat(&res, temp_buf);
     mx_strdel(&temp_buf);
