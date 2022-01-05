4ad21cf1e84fc7d9869e6e2c71b8184d23aaddfa
Rusewww <rusewww1@gmail.com>
2022-01-05 20:57:23 +0200

    fix

--- src/mx_validate_dirs.c
+++ src/mx_validate_dirs.c
@@ -1,17 +1,13 @@
 #include "uls.h"
 
-void mx_validate_dirs(t_list *dirs)
-{
+void mx_validate_dirs(t_list *dirs) {
     DIR *curr;
-    while (dirs)
-    {
+    for (; dirs != NULL; dirs = dirs->next;) {
         curr = opendir(dirs->data);
-        if (curr == NULL && errno != ENOTDIR && errno != EACCES)
-        {
+        if (curr == NULL && errno != ENOTDIR && errno != EACCES) {
             mx_printerr("uls: ");
             perror(dirs->data);
             dirs->data = NULL;
         }
-        dirs = dirs->next;
     }
 }
