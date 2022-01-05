9cd6c88f4cfbb00c51b328bb7496234baef514a8
Rusewww <rusewww1@gmail.com>
2022-01-05 21:41:58 +0200

    fix

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@ char *mx_extract_flags(int argc, char **argv) {
     t_list *uniques = NULL;
     for (int i = 1; i < argc && argv[i][0] == '-'; i++)
         for (int j = 1; argv[i][j] != '\0'; j++)
-            if (is_unique(uniques, argv[i][j]))
+            if (mx_is_unique(uniques, argv[i][j]))
                 mx_push_front(&uniques, &argv[i][j]);
-    res = list_to_str(uniques);
+    res = mx_list_to_str(uniques);
     mx_del_list(&uniques);
     return res;
 }
